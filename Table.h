f0af8c11698227e8557ed910c1c4bf24ff5c5828
Devin Crane <devin.crane@gmail.com>
2017-04-26 11:02:47 -0500

    New Table method

--- Table.h
+++ Table.h
@@ -50,6 +50,9 @@ public:
     {
     }
     ;
+    void setName(string name){
+        sTableName=name;
+    }
     std::vector<std::tuple<int, std::string, bool, std::string, int> > getColumnNames()
     {
         return vColumnNames;
